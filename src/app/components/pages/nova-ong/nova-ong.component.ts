8ada8684dc1b6495f2fb2dfa430d1d4f5d1d8fdb
alexandrejengenhariadev <2008139@aluno.univesp.br>
2022-06-18 09:00:03 -0300

    Criado o serviço de envio

--- src/app/components/pages/nova-ong/nova-ong.component.ts
+++ src/app/components/pages/nova-ong/nova-ong.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Ong } from 'src/app/Ong';
+import { OngService } from 'src/app/services/ong.service';
 
 @Component({
   selector: 'app-nova-ong',
@@ -8,9 +10,25 @@ import { Component, OnInit } from '@angular/core';
 export class NovaOngComponent implements OnInit {
   btnText="Cadastrar";
 
-  constructor() { }
+  constructor(private ongService: OngService) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  async createHandler(ong:Ong){
+    const formData = new FormData();
+    formData.append("nome",ong.nome);
+    formData.append("cep",ong.nome);
+    formData.append("endereco",ong.nome);
+    formData.append("telefone",ong.nome);
+    formData.append("responsavel",ong.nome);
+    formData.append("email",ong.nome);
+    formData.append("imagem",ong.nome);
+   
+    if(ong.imagem){
+      formData.append('imagem',ong.imagem);
+    }
+    await this.ongService.createOng(formData).subscribe();
+    
+
   }
 
 }
